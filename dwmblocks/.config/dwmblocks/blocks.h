1eb1781877afa4bdcaefa31bca883917e43ec84e
kerimcanbalkan <kerimcanbalkan@protonmail.com>
2025-07-08 23:56:18 +0300

    dwmblocks icon spacing fix

--- dwmblocks/.config/dwmblocks/blocks.h
+++ dwmblocks/.config/dwmblocks/blocks.h
@@ -2,7 +2,7 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{" : ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-	{"", "[ \"$(nmcli -t -f WIFI g)\" = enabled ] && echo '󰖩' || echo '󰖪'", 10, 0},
+	{"", "[ \"$(nmcli -t -f WIFI g)\" = enabled ] && echo '󰖩 ' || echo '󰖪' ", 10, 0},
 	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
 };
 
